35be84948f8a63cd4bf14632518628d804550c89
tmadusanka <pvtmadusanka29@gmail.com>
2022-03-24 21:30:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from flask import Flask, request, jsonify 
-
-#from database import *
 import os
-import json
+from flask import Flask
 
-#init app 
 app = Flask(__name__)
 
-@app.route('/')
-def serve(id):
-    service = {'name' : 'thanuja'}
-    json_object = json.dumps(service, indent = 4) 
-    print(json_object)
-    return json_object
 
-if __name__ == "__main__" : 
-    server_port = os.environ.get('PORT', '8080')
-    app.run(debug=False, port=server_port, host='0.0.0.0')
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
